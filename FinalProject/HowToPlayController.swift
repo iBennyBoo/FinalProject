d09ba75cd29fab84ebbf43cb638b8d4708fea840
Ben Berman <bpb062004@gmail.com>
2021-11-29 13:11:58 -0600

    First commit, general layout of app. Edit whatever you want, this is just a template with just tab bar controller and view controllers.

--- FinalProject/HowToPlayController.swift
+++ FinalProject/HowToPlayController.swift
@@ -1,14 +1,13 @@
 import UIKit
-class ViewController: UIViewController {
+class HowToPlayController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let x = CAGradientLayer()
         x.frame = self.view.bounds
-        x.colors = [UIColor.red.cgColor, UIColor.systemYellow.cgColor]
+        x.colors = [UIColor.cyan.cgColor, UIColor.systemYellow.cgColor]
         self.view.layer.insertSublayer(x, at: 0)
     }
-
+   
 
 }
-
