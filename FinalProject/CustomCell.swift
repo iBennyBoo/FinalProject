13104137dad38fedb84d9a2e7d80013d08ad0f49
Ben Berman <bpb062004@gmail.com>
2021-12-01 13:07:00 -0600

    Added all of the assets and created the custom collection view cell... changed it from table view to collection view since it was easier.

---
+++ FinalProject/CustomCell.swift
@@ -0,0 +1,18 @@
+import Foundation
+import UIKit
+public class CustomCell: UICollectionViewCell{
+    
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var labelOutletTwo: UILabel!
+    
+    func configure(first: String, picture: UIImage, second: String){
+        imageOutlet.layer.borderWidth = 0.5
+        labelOutlet.layer.borderWidth = 0.5
+        labelOutletTwo.layer.borderWidth = 0.5
+        
+        labelOutlet?.text = first
+        imageOutlet?.image = picture
+        labelOutletTwo?.text = second
+    }
+}
--- FinalProject/ShopController.swift
+++ FinalProject/ShopController.swift
@@ -1,27 +1,41 @@
 import UIKit
-class ShopController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ShopController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    let items: [UIImage] = []
-    let names: [String] = []
-    let prices: [Int] = []
+    let unlockables: [UIImage] = [UIImage(named: "Default4")!, UIImage(named: "Carl4")!, UIImage(named: "Dwayne4")!, UIImage(named: "Imposter4")!, UIImage(named: "DripSeaver4")!]
+    let names: [String] = ["The Stickman", "The... Ya Know...", "The Rock", "The Sussy Baka", "The Legend"]
+    let prices: [String] = ["0 DABLOONS™", "100 DABLOONS™", "250 DABLOONS™", "500 DABLOONS™", "2500 DABLOONS™"]
     
-    @IBOutlet weak var tableViewOutlet: UITableView!
+    @IBOutlet weak var collectionViewOutlet: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionViewOutlet.delegate = self
+        collectionViewOutlet.dataSource = self
         let x = CAGradientLayer()
         x.frame = self.view.bounds
         x.colors = [UIColor.green.cgColor, UIColor.systemYellow.cgColor]
         self.view.layer.insertSublayer(x, at: 0)
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        32
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return unlockables.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "myCell") as! CustomTableViewCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath) as! CustomCell
+        cell.configure(first: names[indexPath.row], picture: unlockables[indexPath.row], second: prices[indexPath.row])
+        cell.layer.borderWidth = 1
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //print("collectionview")
+        return CGSize(width: 343, height: 140)
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
     }
 
 }
